4ed543c485f257639adafc8e6b83d214c057c1a0
Zalgo <mariozalgo@gmail.com>
2024-08-21 07:25:17 +0200

    N/A: perflib: initial commit, from prometheus-community/windows_exporter

---
+++ perflib/utils.go
@@ -0,0 +1,14 @@
+package perflib
+
+func GetPerflibSnapshot(objNames string) (map[string]*PerfObject, error) {
+	objects, err := QueryPerformanceData(objNames)
+	if err != nil {
+		return nil, err
+	}
+
+	indexed := make(map[string]*PerfObject)
+	for _, obj := range objects {
+		indexed[obj.Name] = obj
+	}
+	return indexed, nil
+}
