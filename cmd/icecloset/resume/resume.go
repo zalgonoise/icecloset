bf3a78099edcdaac6a019e0c8b562c956ec14dda
Zalgo <mariozalgo@gmail.com>
2024-08-22 20:53:26 +0200

    N/A: cmd/icecloset/resume: support PID as target

--- cmd/icecloset/resume/resume.go
+++ cmd/icecloset/resume/resume.go
@@ -18,24 +18,48 @@ func ExecResume(ctx context.Context, logger *slog.Logger, args []string) (int, e
 	fs := flag.NewFlagSet("resume", flag.ExitOnError)
 
 	procName := fs.String("name", "", "the name of the target process")
+	procID := fs.Int("pid", 0, "the process ID of the target process")
 
 	if err := fs.Parse(args); err != nil {
 		return 1, err
 	}
 
-	if *procName == "" {
+	if *procName == "" && *procID < 0 {
 		return 1, ErrTargetProcessRequired
 	}
 
-	if err := Resume(ctx, *procName, logger); err != nil {
+	if *procID > 0 {
+		if err := ResumeProcessByID(*procID); err != nil {
+			return 1, err
+		}
+
+		return 0, nil
+	}
+
+	if err := ResumeProcessByName(ctx, *procName, logger); err != nil {
 		return 1, err
 	}
 
 	return 0, nil
 }
 
-func Resume(ctx context.Context, procName string, logger *slog.Logger) error {
-	h, err := proc.Load(ctx, procName, logger)
+func ResumeProcessByName(ctx context.Context, procName string, logger *slog.Logger) error {
+	h, err := proc.LoadProcessByName(ctx, procName, logger)
+	if err != nil {
+		return err
+	}
+
+	dll := windows.NewLazySystemDLL("ntdll.dll")
+
+	if _, _, err := dll.NewProc("NtResumeProcess").Call(uintptr(h)); !isNilError(err) {
+		return fmt.Errorf("failed to call NtResumeProcess: %w", err)
+	}
+
+	return nil
+}
+
+func ResumeProcessByID(procID int) error {
+	h, err := proc.LoadProcessByID(procID)
 	if err != nil {
 		return err
 	}
